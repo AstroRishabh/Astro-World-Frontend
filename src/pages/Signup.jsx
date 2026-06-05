edb01109baa393bab70bb7e47587004711579134
AstroRishabh <rishabhagarwal032003@gmail.com>
2026-06-05 22:47:46 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("https://astro-world-engine-4.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
