bde16066f5c43dd205efa5398980cf6b826e9ab2
AstroRishabh <rishabhagarwal032003@gmail.com>
2026-06-05 22:49:47 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://astro-world-engine-4.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
